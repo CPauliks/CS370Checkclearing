dcfe8c5a35a3b0d61671e3be69d5559807662904
Christopher Pauliks <massconfuzion@gmail.com>
2011-07-07 20:37:45 -0500

    Now parses single dollars less than 10

--- src/main/java/edu/luc/clearing/CheckClearingServlet.java
+++ src/main/java/edu/luc/clearing/CheckClearingServlet.java
@@ -1,17 +1,53 @@
 package edu.luc.clearing;
 import java.io.IOException;
+import java.io.Reader;
+import java.lang.reflect.Type;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 @SuppressWarnings("serial")
 public class CheckClearingServlet extends HttpServlet {
-    public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setContentType("application/json");
-        resp.getWriter().print(new Gson().toJson(new HashMap<String, Integer>()));
+    private static final HashMap<String, Integer> AMOUNTS = new HashMap<String, Integer>();
+    
+    public CheckClearingServlet() {
+    	AMOUNTS.put("one", 100);
+    	AMOUNTS.put("two", 200);
+    	AMOUNTS.put("three", 300);
+    	AMOUNTS.put("four", 400);
+    	AMOUNTS.put("five", 500);
+    	AMOUNTS.put("six", 600);
+    	AMOUNTS.put("seven", 700);
+    	AMOUNTS.put("eight", 800);
+    	AMOUNTS.put("nine", 900);
     }
+    
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("application/json");
+        resp.getWriter().print(response(req.getReader()));
+    }
+
+	public String response(Reader requestData) {
+		Gson gson = new Gson();
+		HashMap<String,Integer> map = new HashMap<String, Integer>();
+		List<String> checks = gson.fromJson(requestData, requestType());
+		for(String amount: checks){
+			map.put(amount, parseAmount(amount));
+		}
+		return gson.toJson(map);
+	}
+
+	private Type requestType() {
+		return new TypeToken<List<String>>(){}.getType();
+	}
+
+	public Integer parseAmount(String amount) {
+		return AMOUNTS.get(amount);
+	}
 }
--- src/test/java/edu/luc/clearing/CheckClearingServletTest.java
+++ src/test/java/edu/luc/clearing/CheckClearingServletTest.java
@@ -1,10 +1,40 @@
 package edu.luc.clearing;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import java.io.StringReader;
+
+import org.junit.*;
 
 public class CheckClearingServletTest {
+	CheckClearingServlet servlet;
+	
+	@Before
+	public void setup(){
+		servlet = new CheckClearingServlet();
+	}
+	
     @Test
     public void shouldReturnAnEmptyObjectForAnEmptyRequest() throws Exception {
-        new CheckClearingServlet();
+        assertEquals("{}", servlet.response(new StringReader("[]")));
+    }
+    
+    @Test
+    public void shouldReturnCentsForCheckValues() throws Exception{
+    	assertEquals("{\"one\":100}", servlet.response(new StringReader("[\"one\"]")));
+    	assertEquals("{\"seven\":700}", servlet.response(new StringReader("[\"seven\"]")));
+    }
+    
+    @Test
+    public void shouldParseWholeValuesLessThanTen() throws Exception{
+    	assertEquals(100, servlet.parseAmount("one").intValue());
+    	assertEquals(200, servlet.parseAmount("two").intValue());
+    	assertEquals(300, servlet.parseAmount("three").intValue());
+    	assertEquals(400, servlet.parseAmount("four").intValue());
+    	assertEquals(500, servlet.parseAmount("five").intValue());
+    	assertEquals(600, servlet.parseAmount("six").intValue());
+    	assertEquals(700, servlet.parseAmount("seven").intValue());
+    	assertEquals(800, servlet.parseAmount("eight").intValue());
+    	assertEquals(900, servlet.parseAmount("nine").intValue());
     }
 }
