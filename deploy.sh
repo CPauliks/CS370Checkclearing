a0ea465be21eec3f9b1a3ba244301fdf38695e09
Christopher Pauliks <massconfuzion@gmail.com>
2011-08-04 20:05:27 -0500

    still looking at fixing deploy

--- deploy.sh
+++ deploy.sh
@@ -1,79 +1,73 @@
 #!/bin/bash
 
-set -e
-
 function cleanup {
-	kill $server_pid
-	rm gradle.properties
-	[[]]
+  kill $server_pid
+  rm gradle.properties
+  exit 0
 }
 
 trap "cleanup" INT TERM EXIT
 
 function unknown_files {
-	unknown_file_count=`git status --porcelain | grep "^??" | wc -l`
-	[[ "$unknown_file_count" -gt 0 ]]
+  unknown_file_count=`git status --porcelain | grep "^??" | wc -l`
+  [[ "$unknown_file_count" -gt 0 ]]
 }
 
-function uncommmitted_changes {
-	changed_committed=`git diff HEAD --shortstat | wc -l`
-	[[ "$changed_committed" -gt 0 ]]
+function uncommitted_changes {
+  count=`git diff HEAD --shortstat | wc -l`
+  [[ "$count" -gt 0 ]]
 }
 
 if unknown_files; then
-	echo "Unknown files in project!"
-	exit 1
+  echo "Unknown files in project!"
+  exit 1
 fi
 
-if uncommmitted_changes; then
-	echo "Uncommitted files in project"
-	exit 1
+if uncommitted_changes; then
+  echo "Uncommitted files in project!"
+  exit 1
 fi
 
 gradle clean build
-
 if [ "$?" -gt 0 ]; then
-	exit 1
+  exit 1
 fi
 
 gradle gaeRun &
 server_pid=$!
-
 if [ "$?" -gt 0 ]; then
-	echo "Server failed to start!"
-	exit 1
+  #add count to while loop for optional timeout
+  echo "Server fialed to start"
+  exit 1
 fi
 
 server_status=1
-
 echo -n "Waiting for local server to start..."
-
 while [ $server_status -gt 0 ]; do
-	echo -n .
-	curl http://localhost:8085
-	server_status=$?
-	sleep 1
+  echo -n
+  curl http://localhost:8085
+  server_status=$?
+  sleep 1
 done
 
-history=`curl -s http://cpaulikscs370.appspot.com/checkclearing`
-
+history=`curl http://dgladecs370.appspot.com/checkclearing`
 response=`curl -s -H Content-Type:application/json -d "$history" http://localhost:8085/checkclearing`
 
 echo $response | python -mjson.tool > /dev/null
-
 if [ "$?" -eq 0 ]; then
-	echo "Response is valid JSON"
+  echo "Response is valid JSON"
 else
-	echo "Response is not valid JSON"
-	echo $response
-	exit 1
+  echo "Response is NOT valid JSON"
+  echo $response
+  exit 1
 fi
 
 kill $server_pid
+wait $server_pid
 
 echo "Build successful! Enter AppEngine password to deploy"
 stty -echo
-read -p "Password:  " password
+read -p "Password: " password
 echo
 stty echo
 
@@ -82,6 +76,7 @@ gradle gaeUpload
 rm gradle.properties
 
 timestamp=`date "+%Y-%m-%d_%H_%M_%S"`
+echo $timestamp
 git tag DEPLOY_$timestamp
 
 echo "Exiting..."
